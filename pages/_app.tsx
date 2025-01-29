598c45b6e1f6f31ef63877378bd6cc99514df5f1
ArturoCCS <ccatschark@gmail.com>
2025-01-28 23:36:39 -0600

    update 28-01

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,38 @@
+import { ThemeProvider } from "next-themes"; // Importar ThemeProvider
 import { useEffect, useState } from "react";
 import IntroScreen from "../components/IntroScreen";
 
 function MyApp({ Component, pageProps }) {
-  const [showIntro, setShowIntro] = useState(true);
+  const [showIntro, setShowIntro] = useState(null); // Inicializar como null
 
   useEffect(() => {
-    // const hasSeenIntro = localStorage.getItem("hasSeenIntro"); 
-    const hasSeenIntro = localStorage.getItem("");
-    if (hasSeenIntro) {
-      setShowIntro(false);
+      if (typeof window !== "undefined") {
+      // const hasSeenIntro = localStorage.getItem("");
+      const hasSeenIntro = localStorage.getItem("hasSeenIntro");
+      setShowIntro(!hasSeenIntro); 
     }
   }, []);
 
   const handleContinue = () => {
-    localStorage.setItem("hasSeenIntro", "true");
+    if (typeof window !== "undefined") {
+      localStorage.setItem("hasSeenIntro", "true");
+    }
     setShowIntro(false);
   };
 
+  // No renderizar nada hasta que se determine si mostrar la intro
+  if (showIntro === null) {
+    return null;
+  }
+
   return (
-    <>
+    <ThemeProvider attribute="class" defaultTheme="system">
       {showIntro ? (
         <IntroScreen onContinue={handleContinue} />
-        
       ) : (
         <Component {...pageProps} />
       )}
-    </>
+    </ThemeProvider>
   );
 }
 
